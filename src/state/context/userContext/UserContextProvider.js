a0f2e59bdc7736124d8236b803d49ee76b9a130f
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2023-04-29 22:19:11 +0530

    wdff

--- src/state/context/userContext/UserContextProvider.js
+++ src/state/context/userContext/UserContextProvider.js
@@ -1,12 +1,29 @@
 import React from "react";
-import {UserContext} from "./UserContext.js"
-const UserContextProvider = ({children})=>{
-    
-    return(
-        <UserContext.provider value={}>
-            {children}
-            </UserContext.provider>
-    );
-}
+import { UserContext } from "./UserContext.js";
+import { auth } from "../../../config/fireabse.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+const UserContextProvider = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+  return (
+    <UserContext.Provider value={{ createUser, signIn, logOut }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContextProvider;
